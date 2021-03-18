6eeae3340b3b28f464870dd913beb886fb1590f1
Charlotte <dan.jones8902@gmail.com>
2021-03-18 20:01:54 +0000

    Added redementry pick up system

---
+++ Assets/Scripts/Player/PickUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+	public GameObject modle;
+
+	bool pickedUp;
+
+	public static int counter;
+	PickUpCounter pc;
+
+	private void Start()
+	{
+		pc = GameObject.Find("Canvas").GetComponent<PickUpCounter>();
+	}
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			counter++;
+			pickedUp = true;
+			pc.IncreaseCounter();
+			modle.SetActive(false);
+		}
+	}
+}
