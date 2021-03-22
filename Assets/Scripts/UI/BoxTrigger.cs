3d9d3b9612aca72e3531b110d2a553e5bf3898a9
Charlotte <dan.jones8902@gmail.com>
2021-03-22 19:03:22 +0000

    Added main menu and trigger box for end screen

---
+++ Assets/Scripts/UI/BoxTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxTrigger : MonoBehaviour
+{
+	public GameObject endScreen;
+
+	private void Start()
+	{
+		if (endScreen != null)
+			endScreen = Instantiate(endScreen);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player") && endScreen != null)
+			endScreen.SetActive(true);
+	}
+}
