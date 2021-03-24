13c980b4dd05d18af932b6cf39b51576cfa47150
Charlotte <dan.jones8902@gmail.com>
2021-03-24 17:56:30 +0000

    Started doing the child ai
    The child will spend a short amount of time in a room with no player and a longer amount of time when they are in the same room

--- Assets/Scripts/ChildAI/ChildAI.cs
+++ Assets/Scripts/ChildAI/ChildAI.cs
@@ -3,25 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum AIState
+{
+    Idle, // Doesn't know where the player is
+    Alert, // Knows the player is in the room
+    Attack // Knows where the player is and is moving to kill
+}
+
 
 public class ChildAI : MonoBehaviour
 {
-    public Transform waypointList;
+    public Transform[] waypointRooms;
     public NavMeshAgent agent;
 
-    public float stayTime;
-    private float endStayTime;
+	public float timeInRoom;
+	public float timeInRoomWithPlayer;
+
+	public float moveSpeed;
+	public float speedWithPlayerInRoom;
+
+	public float timeAtWaypoint;
+	public float timeAtWaypointWithPlayer;
+
+
+	private float eTimeRoom;
+	private float eTimeRoomPlayer;
+
+	private float eTimeWaypoint;
+	private float eTimeWayPlayer;
 
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.time >= endStayTime)
-		{
-            endStayTime = Time.time + stayTime;
+		SwitchState(AIState.Idle);
+	}
 
-            // travel to random waypoint
-            agent.SetDestination(waypointList.GetChild(Random.Range(0, waypointList.childCount)).position);
+	public void SwitchState(AIState state)
+	{
+		switch (state)
+		{
+			case AIState.Idle:
+				Idle();
+				break;
+			case AIState.Alert:
+				Alert();
+				break;
+			case AIState.Attack:
+				Attack();
+				break;
 		}
-    }
+	}
+
+
+    public void Idle()
+	{
+		// Child is in the same room as the player
+		if (EntityFinder.ChildLocation == EntityFinder.PlayerLocation)
+		{
+			// Pick a random waypoint in that room and stay there for a few seconds
+
+
+
+		}
+		else
+		{
+
+		}
+	}
+
+    public void Alert()
+	{
+
+	}
+
+    public void Attack()
+	{
+
+	}
 }
---
+++ Assets/Scripts/World/EntityFinder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityFinder : MonoBehaviour
+{
+    public static Transform PlayerLocation;
+    public static Transform ChildLocation;
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			PlayerLocation = transform;
+		}
+
+		if (other.CompareTag("Child"))
+		{
+			ChildLocation = transform;
+		}
+	}
+}
